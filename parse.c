6250a345b4718672261320f5a8957f716af69abc
majormil <majormil@debian.zero.net>
2024-09-04 23:56:26 +0200

    Code cleanup of parse.c

--- parse.c
+++ parse.c
@@ -18,244 +18,258 @@
 
 #define MAX_LINE 1024
 
-static bool debug_mode = false;
+static bool debug = false;
 
-static int execute_command(char *cmd);
-static char *trim_whitespace(char *str);
-static char *parse_quoted_string(char **cmd);
-static void handle_command_chain(char *line);
+/* Function Prototypes */
+static int exec_cmd(char *cmd);
+static char *trim(char *str);
+static char *parse_arg(char **cmd);
+static void handle_chain(char *line);
+static void handle_result(char sep, int status, bool *exec_next);
+static size_t append_env_var(char **res, size_t *res_len, const char *env_name);
+static int exec_external(char *args[]);
 
-// Check for strndup availability
-#ifndef HAVE_STRNDUP
-char *strndup(const char *str, size_t n) {
-    char *result = malloc(n + 1);
-    if (result) {
-        strncpy(result, str, n);
-        result[n] = '\0';
+/* Custom strndup implementation */
+static char *
+strndup(const char *s, size_t n)
+{
+    char *p;
+    size_t len = strnlen(s, n);
+    p = malloc(len + 1);
+    if (p) {
+        memcpy(p, s, len);
+        p[len] = '\0';
     }
-    return result;
-}
-#endif
-
-void set_debug_mode(bool mode) {
-    debug_mode = mode;
-}
-
-void parse_and_execute(char *line) {
-    handle_command_chain(line);
+    return p;
 }
 
-static void handle_command_chain(char *line) {
-    char *cmd_start = line;
-    char *cmd_end;
-    int status = 0;
-    bool execute_next = true;
-
-    while (*cmd_start) {
-        cmd_start = trim_whitespace(cmd_start);
-        if (!*cmd_start)
-            break;
-
-        cmd_end = cmd_start + strcspn(cmd_start, ";&|");
-
-        if (execute_next) {
-            char *command = strndup(cmd_start, cmd_end - cmd_start);
-            if (!command) {
-                perror("strndup");
-                exit(EXIT_FAILURE);
-            }
-            
-            char *expanded_command = expand_variables(command);
-            free(command);
-            if (!expanded_command) {
-                fprintf(stderr, "Failed to expand command\n");
-                exit(EXIT_FAILURE);
-            }
-
-            status = execute_command(expanded_command);
-            free(expanded_command);
-        }
-
-        cmd_start = cmd_end + 1;
-
-        if (*cmd_end == '&') {
-            execute_next = (status == 0);
-            if (*(cmd_end + 1) == '&')
-                cmd_start++;
-        } else if (*cmd_end == '|') {
-            execute_next = (status != 0);
-            if (*(cmd_end + 1) == '|')
-                cmd_start++;
-        } else {
-            execute_next = true;
-        }
-    }
+void
+set_debug(bool mode)
+{
+	debug = mode;
 }
 
-static int execute_command(char *cmd) {
-    char *args[MAX_LINE / 2 + 1];
-    char *arg = cmd;
-    int i = 0;
-
-    while (*arg) {
-        arg = trim_whitespace(arg);
-        if (!*arg)
-            break;
-
-        args[i++] = parse_quoted_string(&arg);
-        if (!args[i-1]) {
-            fprintf(stderr, "Failed to parse argument\n");
-            return -1;
-        }
-    }
-    args[i] = NULL;
-
-    if (!args[0])
-        return 0;
-
-    if (debug_mode) {
-        printf("Executing command: %s\n", args[0]);
-        for (int j = 0; j < i; j++) {
-            printf("arg[%d]: %s\n", j, args[j]);
-        }
-    }
-
-    if (strcmp(args[0], "exit") == 0)
-        exit(0);
-
-    add_to_history(args[0]);
-
-    if (is_builtin(args[0])) {
-        run_builtin(args);
-        return last_exit_status;
-    }
-
-    pid_t pid = fork();
-    if (pid == 0) {
-        execvp(args[0], args);
-        perror("shush");
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        perror("shush: fork failed");
-        return -1;
-    } else {
-        int status;
-        waitpid(pid, &status, 0);
-        return WIFEXITED(status) ? WEXITSTATUS(status) : 1;
-    }
+void
+parse_and_execute(char *line)
+{
+	handle_chain(line);
 }
 
-static char *trim_whitespace(char *str) {
-    while (isspace((unsigned char)*str))
-        str++;
-    if (*str == 0)
-        return str;
+static void
+handle_chain(char *line)
+{
+	char *cmd, *end;
+	int status = 0;
+	bool exec_next = true;
 
-    char *end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end))
-        end--;
+	while (*line) {
+		line = trim(line);
+		if (!*line)
+			break;
 
-    end[1] = '\0';
-    return str;
+		end = line + strcspn(line, ";&|");
+
+		if (exec_next) {
+			cmd = strndup(line, end - line);
+			if (!cmd) {
+				perror("strndup");
+				exit(1);
+			}
+			char *expanded = expand_variables(cmd); /* Reverting to original name */
+			free(cmd);
+
+			if (!expanded) {
+				fprintf(stderr, "Failed to expand command\n");
+				exit(1);
+			}
+
+			status = exec_cmd(expanded);
+			free(expanded);
+		}
+
+		handle_result(*end, status, &exec_next);
+		line = *end ? end + 1 : end;
+	}
 }
 
-static char *parse_quoted_string(char **cmd) {
-    char *str = *cmd;
-    char *start = str;
-    bool in_quotes = false;
-
-    while (*str && (in_quotes || !isspace((unsigned char)*str))) {
-        if (*str == '"')
-            in_quotes = !in_quotes;
-        else if (*str == '\\' && *(str + 1) != '\0')
-            str++;
-        str++;
-    }
-
-    char *token = strndup(start, str - start);
-    if (!token) {
-        perror("strndup");
-        exit(EXIT_FAILURE);
-    }
-
-    *cmd = *str ? str + 1 : str;
-    return token;
+static void
+handle_result(char sep, int status, bool *exec_next)
+{
+	switch (sep) {
+	case '&':
+		*exec_next = (status == 0);
+		break;
+	case '|':
+		*exec_next = (status != 0);
+		break;
+	default:
+		*exec_next = true;
+		break;
+	}
 }
 
-char *expand_variables(const char *input) {
-    if (!home_directory) {
-        fprintf(stderr, "Error: HOME is not set\n");
-        exit(EXIT_FAILURE);
-    }
+static int
+exec_cmd(char *cmd)
+{
+	char *args[MAX_LINE / 2 + 1];
+	int i = 0;
 
-    size_t len = strlen(input);
-    char *expanded = malloc(len + 1);
-    if (!expanded) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    strcpy(expanded, input);
+	while (*cmd) {
+		cmd = trim(cmd);
+		if (!*cmd)
+			break;
 
-    char *result = malloc(len + 1);
-    if (!result) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    size_t result_len = 0;
+		args[i++] = parse_arg(&cmd);
+		if (!args[i - 1]) {
+			fprintf(stderr, "Failed to parse argument\n");
+			return -1;
+		}
+	}
+	args[i] = NULL;
 
-    for (size_t i = 0; i < len; i++) {
-        if (expanded[i] == '~') {
-            size_t home_len = strlen(home_directory);
-            char *new_result = realloc(result, result_len + home_len + 1);
-            if (!new_result) {
-                perror("realloc");
-                free(result);
-                exit(EXIT_FAILURE);
-            }
-            result = new_result;
-            strcpy(result + result_len, home_directory);
-            result_len += home_len;
-        } else if (expanded[i] == '$' && i + 1 < len) {
-            char *env_var_name = expanded + i + 1;
-            char *end = env_var_name;
+	if (!args[0])
+		return 0;
 
-            while (*end && (isalnum(*end) || *end == '_'))
-                end++;
+	if (debug) {
+		printf("Executing: %s\n", args[0]);
+		for (int j = 0; j < i; j++)
+			printf("arg[%d]: %s\n", j, args[j]);
+	}
 
-            size_t var_name_len = end - env_var_name;
-            if (var_name_len > 0) {
-                char var_name[var_name_len + 1];
-                strncpy(var_name, env_var_name, var_name_len);
-                var_name[var_name_len] = '\0';
+	if (!strcmp(args[0], "exit"))
+		exit(0);
 
-                char *var_value = getenv(var_name);
-                if (var_value) {
-                    size_t value_len = strlen(var_value);
-                    char *new_result = realloc(result, result_len + value_len + 1);
-                    if (!new_result) {
-                        perror("realloc");
-                        free(result);
-                        exit(EXIT_FAILURE);
-                    }
-                    result = new_result;
-                    strcpy(result + result_len, var_value);
-                    result_len += value_len;
-                }
-                i += var_name_len;
-            }
-        } else {
-            char *new_result = realloc(result, result_len + 2);
-            if (!new_result) {
-                perror("realloc");
-                free(result);
-                exit(EXIT_FAILURE);
-            }
-            result = new_result;
-            result[result_len++] = expanded[i];
-        }
-    }
+	add_to_history(args[0]); /* Reverting to original name */
 
-    result[result_len] = '\0';
-    free(expanded);
-    return result;
+	if (is_builtin(args[0])) {
+		run_builtin(args);
+		return last_exit_status;
+	}
+
+	return exec_external(args); /* Ensure this function matches the declaration */
+}
+
+static int
+exec_external(char *args[])
+{
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		execvp(args[0], args);
+		perror("shush");
+		exit(1);
+	} else if (pid < 0) {
+		perror("shush: fork failed");
+		return -1;
+	} else {
+		int status;
+		waitpid(pid, &status, 0);
+		return WIFEXITED(status) ? WEXITSTATUS(status) : 1;
+	}
+}
+
+static char *
+trim(char *str)
+{
+	while (isspace((unsigned char)*str))
+		str++;
+	if (*str == 0)
+		return str;
+
+	char *end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end))
+		end--;
+
+	end[1] = '\0';
+	return str;
+}
+
+static char *
+parse_arg(char **cmd)
+{
+	char *str = *cmd, *start = str;
+	bool quoted = false;
+
+	while (*str && (quoted || !isspace((unsigned char)*str))) {
+		if (*str == '"')
+			quoted = !quoted;
+		else if (*str == '\\' && *(str + 1))
+			str++;
+		str++;
+	}
+
+	char *tok = strndup(start, str - start);
+	if (!tok) {
+		perror("strndup");
+		exit(1);
+	}
+
+	*cmd = *str ? str + 1 : str;
+	return tok;
+}
+
+char *
+expand_variables(const char *input) /* Reverting to original name */
+{
+	if (!home_directory) {
+		fprintf(stderr, "Error: HOME not set\n");
+		exit(1);
+	}
+
+	size_t len = strlen(input);
+	char *res = malloc(len + 1);
+	if (!res) {
+		perror("malloc");
+		exit(1);
+	}
+
+	size_t res_len = 0;
+	for (size_t i = 0; i < len; i++) {
+		if (input[i] == '~') {
+			size_t home_len = strlen(home_directory);
+			res = realloc(res, res_len + home_len + 1);
+			if (!res) {
+				perror("realloc");
+				exit(1);
+			}
+			strcpy(res + res_len, home_directory);
+			res_len += home_len;
+		} else if (input[i] == '$' && i + 1 < len) {
+			i += append_env_var(&res, &res_len, input + i + 1);
+		} else {
+			res[res_len++] = input[i];
+		}
+	}
+
+	res[res_len] = '\0';
+	return res;
+}
+
+static size_t
+append_env_var(char **res, size_t *res_len, const char *env_name)
+{
+	const char *end = env_name;
+
+	while (*end && (isalnum(*end) || *end == '_'))
+		end++;
+
+	size_t name_len = end - env_name;
+	if (name_len) {
+		char var[name_len + 1];
+		strncpy(var, env_name, name_len);
+		var[name_len] = '\0';
+
+		char *val = getenv(var);
+		if (val) {
+			size_t val_len = strlen(val);
+			*res = realloc(*res, *res_len + val_len + 1);
+			if (!*res) {
+				perror("realloc");
+				exit(1);
+			}
+			strcpy(*res + *res_len, val);
+			*res_len += val_len;
+		}
+	}
+	return name_len;
 }
