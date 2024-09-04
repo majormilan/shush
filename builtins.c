914282c1af9aeadac034d67a5dbd25ab3db9ff34
majormil <majormil@debian.zero.net>
2024-09-04 18:56:18 +0200

    Better error handling, and more robust code

--- builtins.c
+++ builtins.c
@@ -12,7 +12,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
-
 #include "builtins.h"
 #include "init.h"
 #include "parse.h"
@@ -53,10 +52,48 @@ void builtin_alias(char *args[]);
 void builtin_unalias(char *args[]);
 void builtin_source(char *args[]);
 
+const char *custom_strsignal(int sig) {
+    static const char *signals[] = {
+        [SIGHUP] = "Hangup",
+        [SIGINT] = "Interrupt",
+        [SIGQUIT] = "Quit",
+        [SIGILL] = "Illegal instruction",
+        [SIGTRAP] = "Trace/breakpoint trap",
+        [SIGABRT] = "Aborted",
+        [SIGBUS] = "Bus error",
+        [SIGFPE] = "Floating point exception",
+        [SIGKILL] = "Killed",
+        [SIGUSR1] = "User defined signal 1",
+        [SIGSEGV] = "Segmentation fault",
+        [SIGUSR2] = "User defined signal 2",
+        [SIGPIPE] = "Broken pipe",
+        [SIGALRM] = "Alarm clock",
+        [SIGTERM] = "Terminated",
+        [SIGSTKFLT] = "Stack fault",
+        [SIGCHLD] = "Child exited",
+        [SIGCONT] = "Continue",
+        [SIGSTOP] = "Stop",
+        [SIGTSTP] = "Terminal stop",
+        [SIGTTIN] = "Background read from tty",
+        [SIGTTOU] = "Background write to tty",
+        [SIGURG] = "Urgent condition on socket",
+        [SIGXCPU] = "CPU time limit exceeded",
+        [SIGXFSZ] = "File size limit exceeded",
+        [SIGVTALRM] = "Virtual alarm clock",
+        [SIGPROF] = "Profiling timer expired",
+        [SIGWINCH] = "Window size change",
+        [SIGIO] = "I/O possible",
+        [SIGPWR] = "Power failure",
+        [SIGSYS] = "Bad system call",
+    };
+
+    if (sig >= 1 && sig < sizeof(signals) / sizeof(signals[0]) && signals[sig])
+        return signals[sig];
+    return "Unknown signal";
+}
+
 /* Add a command to history */
-void
-add_to_history(const char *command)
-{
+void add_to_history(const char *command) {
     if (history_count < MAX_HISTORY) {
         history[history_count++] = strdup(command);
     } else {
@@ -67,22 +104,17 @@ add_to_history(const char *command)
 }
 
 /* Check if a command is a built-in */
-bool
-is_builtin(const char *command)
-{
-    for (int i = 0; command_table[i].name != NULL; i++) {
-        if (!strcmp(command, command_table[i].name)) {
+bool is_builtin(const char *command) {
+    for (int i = 0; command_table[i].name; i++) {
+        if (!strcmp(command, command_table[i].name))
             return true;
-        }
     }
     return false;
 }
 
 /* Run a built-in command */
-void
-run_builtin(char *args[])
-{
-    for (int i = 0; command_table[i].name != NULL; i++) {
+void run_builtin(char *args[]) {
+    for (int i = 0; command_table[i].name; i++) {
         if (!strcmp(args[0], command_table[i].name)) {
             command_table[i].func(args);
             return;
@@ -93,24 +125,20 @@ run_builtin(char *args[])
 }
 
 /* Built-in echo command */
-void
-builtin_echo(char *args[])
-{
-    int newline = 1;
-    int interpret_escapes = 0;
-    int i = 1;
+void builtin_echo(char *args[]) {
+    int newline = 1, interpret_escapes = 0, i = 1;
 
     while (args[i]) {
-        if (strcmp(args[i], "-n") == 0) {
+        if (!strcmp(args[i], "-n")) {
             newline = 0;
             i++;
-        } else if (strcmp(args[i], "-e") == 0) {
+        } else if (!strcmp(args[i], "-e")) {
             interpret_escapes = 1;
             i++;
-        } else if (strcmp(args[i], "-E") == 0) {
+        } else if (!strcmp(args[i], "-E")) {
             interpret_escapes = 0;
             i++;
-        } else if (strcmp(args[i], "--help") == 0) {
+        } else if (!strcmp(args[i], "--help")) {
             printf("echo: echo [-neE] [string ...]\n");
             printf("    Write arguments to the standard output.\n\n");
             printf("    Options:\n");
@@ -119,9 +147,8 @@ builtin_echo(char *args[])
             printf("      -E    disable interpretation of backslash escapes (default)\n");
             last_exit_status = 0;
             return;
-        } else {
+        } else
             break;
-        }
     }
 
     if (args[i] && args[i][0] == '-' && args[i][1] != '\0') {
@@ -158,27 +185,22 @@ builtin_echo(char *args[])
 
         free(expanded_arg);
 
-        if (args[i + 1]) {
+        if (args[i + 1])
             putchar(' ');
-        }
     }
 
-    if (newline) {
+    if (newline)
         putchar('\n');
-    }
 
     last_exit_status = 0;
 }
 
 /* Built-in history command */
-void
-builtin_history(char *args[])
-{
+void builtin_history(char *args[]) {
     if (args[1]) {
         if (!strcmp(args[1], "-c")) {
-            for (int i = 0; i < history_count; i++) {
+            for (int i = 0; i < history_count; i++)
                 free(history[i]);
-            }
             history_count = 0;
             last_exit_status = 0;
         } else if (!strcmp(args[1], "-d") && args[2]) {
@@ -197,35 +219,29 @@ builtin_history(char *args[])
             last_exit_status = 1;
         }
     } else {
-        for (int i = 0; i < history_count; i++) {
+        for (int i = 0; i < history_count; i++)
             printf("%d %s\n", i + 1, history[i]);
-        }
     }
 }
 
 /* Built-in cd command */
-void
-builtin_cd(char *args[])
-{
-    char *target_dir = NULL;
-    if (args[1] == NULL) {
-        target_dir = home_directory;
-    } else if (strcmp(args[1], "-") == 0) {
-        target_dir = getenv("OLDPWD");
-        if (target_dir) {
-            printf("%s\n", target_dir);
-        }
-    } else {
-        target_dir = args[1];
-    }
+void builtin_cd(char *args[]) {
+    char cwd[1024];
+    char *target_dir = args[1] ? (strcmp(args[1], "-") == 0 ? getenv("OLDPWD") : args[1]) : home_directory;
 
     if (target_dir) {
         if (chdir(target_dir) != 0) {
             perror("shush");
             last_exit_status = 1;
         } else {
-            setenv("OLDPWD", getenv("PWD"), 1);
-            setenv("PWD", getcwd(NULL, 0), 1);
+            char *pwd = getcwd(cwd, sizeof(cwd));
+            if (pwd) {
+                setenv("OLDPWD", getenv("PWD"), 1);
+                setenv("PWD", pwd, 1);
+            } else {
+                perror("getcwd");
+                last_exit_status = 1;
+            }
             last_exit_status = 0;
         }
     } else {
@@ -235,22 +251,18 @@ builtin_cd(char *args[])
 }
 
 /* Built-in ver command */
-void
-builtin_ver(char *args[])
-{
+void builtin_ver(char *args[]) {
     printf("shush version 1.0\n");
     last_exit_status = 0;
 }
 
 /* Built-in exit command */
-void
-builtin_exit(char *args[])
-{
+void builtin_exit(char *args[]) {
     int status = last_exit_status;
     if (args[1]) {
         char *endptr;
         status = strtol(args[1], &endptr, 10);
-        if (*endptr != '\0') {
+        if (*endptr) {
             fprintf(stderr, "exit: %s: numeric argument required\n", args[1]);
             status = 1;
         }
@@ -259,27 +271,21 @@ builtin_exit(char *args[])
 }
 
 /* Built-in pwd command */
-void
-builtin_pwd(char *args[])
-{
-    int logical = 1;
-    if (args[1] && strcmp(args[1], "-P") == 0) {
-        logical = 0;
-    }
-
+void builtin_pwd(char *args[]) {
+    int logical = args[1] && !strcmp(args[1], "-P");
     if (logical) {
-        char *pwd = getenv("PWD");
-        if (pwd) {
-            printf("%s\n", pwd);
-        } else {
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)))
+            printf("%s\n", cwd);
+        else {
             perror("pwd");
             last_exit_status = 1;
         }
     } else {
-        char cwd[1024];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            printf("%s\n", cwd);
-        } else {
+        char *pwd = getenv("PWD");
+        if (pwd)
+            printf("%s\n", pwd);
+        else {
             perror("pwd");
             last_exit_status = 1;
         }
@@ -287,27 +293,22 @@ builtin_pwd(char *args[])
 }
 
 /* Built-in set command */
-void
-builtin_set(char *args[])
-{
-    if (args[1] == NULL) {
-        extern char **environ;
-        for (char **env = environ; *env; ++env) {
-            printf("%s\n", *env);
-        }
-        last_exit_status = 0;
-    } else {
+void builtin_set(char *args[]) {
+    if (args[1]) {
         fprintf(stderr, "set: Invalid usage\n");
         last_exit_status = 1;
+    } else {
+        extern char **environ;
+        for (char **env = environ; *env; ++env)
+            printf("%s\n", *env);
+        last_exit_status = 0;
     }
 }
 
 /* Built-in unset command */
-void
-builtin_unset(char *args[])
-{
+void builtin_unset(char *args[]) {
     for (int i = 1; args[i]; i++) {
-        if (unsetenv(args[i]) != 0) {
+        if (unsetenv(args[i])) {
             fprintf(stderr, "unset: %s: cannot unset\n", args[i]);
             last_exit_status = 1;
         }
@@ -316,26 +317,27 @@ builtin_unset(char *args[])
 }
 
 /* Built-in export command */
-void
-builtin_export(char *args[])
-{
+void builtin_export(char *args[]) {
     if (!args[1]) {
         extern char **environ;
-        for (char **env = environ; *env; ++env) {
+        for (char **env = environ; *env; ++env)
             printf("declare -x %s\n", *env);
-        }
         last_exit_status = 0;
     } else {
         for (int i = 1; args[i]; i++) {
             char *equal_sign = strchr(args[i], '=');
             if (equal_sign) {
                 *equal_sign = '\0';
-                setenv(args[i], equal_sign + 1, 1);
+                if (setenv(args[i], equal_sign + 1, 1)) {
+                    perror("export");
+                    last_exit_status = 1;
+                }
             } else {
                 if (getenv(args[i])) {
                     printf("declare -x %s=\"%s\"\n", args[i], getenv(args[i]));
-                } else {
-                    setenv(args[i], "", 1);
+                } else if (setenv(args[i], "", 1)) {
+                    perror("export");
+                    last_exit_status = 1;
                 }
             }
         }
@@ -344,17 +346,13 @@ builtin_export(char *args[])
 }
 
 /* Built-in kill command */
-void
-builtin_kill(char *args[])
-{
-    int signal = SIGTERM;
-    int arg_index = 1;
+void builtin_kill(char *args[]) {
+    int signal = SIGTERM, arg_index = 1;
 
     if (args[1]) {
-        if (strcmp(args[1], "-l") == 0) {
-            for (int i = 1; i < NSIG; i++) {
-                printf("%s ", strsignal(i));
-            }
+        if (!strcmp(args[1], "-l")) {
+            for (int i = 1; i < NSIG; i++)
+                printf("%s ", custom_strsignal(i));
             printf("\n");
             last_exit_status = 0;
             return;
@@ -372,7 +370,7 @@ builtin_kill(char *args[])
 
     for (int i = arg_index; args[i]; i++) {
         pid_t pid = atoi(args[i]);
-        if (kill(pid, signal) != 0) {
+        if (kill(pid, signal)) {
             perror("kill");
             last_exit_status = 1;
         }
@@ -382,20 +380,17 @@ builtin_kill(char *args[])
 }
 
 /* Built-in alias command */
-void
-builtin_alias(char *args[])
-{
-    if (args[1] == NULL) {
-        for (int i = 0; i < alias_count; i++) {
+void builtin_alias(char *args[]) {
+    if (!args[1]) {
+        for (int i = 0; i < alias_count; i++)
             printf("alias %s='%s'\n", aliases[i].name, aliases[i].value);
-        }
     } else {
         for (int i = 1; args[i]; i++) {
             char *equal_sign = strchr(args[i], '=');
             if (equal_sign) {
                 *equal_sign = '\0';
                 for (int j = 0; j < alias_count; j++) {
-                    if (strcmp(aliases[j].name, args[i]) == 0) {
+                    if (!strcmp(aliases[j].name, args[i])) {
                         free(aliases[j].value);
                         aliases[j].value = strdup(equal_sign + 1);
                         goto next_arg;
@@ -406,7 +401,7 @@ builtin_alias(char *args[])
                 alias_count++;
             } else {
                 for (int j = 0; j < alias_count; j++) {
-                    if (strcmp(aliases[j].name, args[i]) == 0) {
+                    if (!strcmp(aliases[j].name, args[i])) {
                         printf("alias %s='%s'\n", aliases[j].name, aliases[j].value);
                     }
                 }
@@ -419,10 +414,8 @@ builtin_alias(char *args[])
 }
 
 /* Built-in unalias command */
-void
-builtin_unalias(char *args[])
-{
-    if (args[1] == NULL) {
+void builtin_unalias(char *args[]) {
+    if (!args[1]) {
         fprintf(stderr, "unalias: usage: unalias name [name ...]\n");
         last_exit_status = 1;
         return;
@@ -430,7 +423,7 @@ builtin_unalias(char *args[])
 
     for (int i = 1; args[i]; i++) {
         for (int j = 0; j < alias_count; j++) {
-            if (strcmp(aliases[j].name, args[i]) == 0) {
+            if (!strcmp(aliases[j].name, args[i])) {
                 free(aliases[j].name);
                 free(aliases[j].value);
                 memmove(&aliases[j], &aliases[j + 1], (alias_count - j - 1) * sizeof(alias_t));
@@ -444,9 +437,7 @@ builtin_unalias(char *args[])
 }
 
 /* Built-in source command */
-void
-builtin_source(char *args[])
-{
+void builtin_source(char *args[]) {
     if (args[1]) {
         FILE *file = fopen(args[1], "r");
         if (!file) {
